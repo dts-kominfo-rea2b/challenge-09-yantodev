85f42a042c4be67dfb1129200dcecea43c2e7b8c
yanto <ekocahyanto007@gmail.com>
2022-06-30 19:38:36 +0700

    challange-09

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,18 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css';
 
-const Contact = () => {
+const Contact = ({ data }) => {
     return (
-        <></>
+        <div class="row">
+            <img src={data.photo} alt='profile-pict' />
+            <div class="description">
+                <h4>{data.name}</h4>
+                <p>{data.phone}</p>
+                <p>{data.email}</p>
+            </div>
+        </div>
     )
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 // Kalian bisa menambahkan CSS di src/components/Header.css
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
     return (
-        <></>
+        <header>
+            <h1>Call a Friend</h1>
+            <p>Your friendly contact app</p>
+            <hr />
+        </header>
     )
 }
 
